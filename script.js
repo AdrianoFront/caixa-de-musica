f2c43cf0071a64a80b4b592cedd5259ddfd476dd
Adriano <98757084+AdrianoFront@users.noreply.github.com>
2022-05-25 22:20:49 -0300

    create files for constrution

---
+++ script.js
@@ -0,0 +1,7 @@
+let toast = document.querySelector(".toast-container");
+let botaoCadastrar = document.querySelector("#botaoCadastrar");
+console;
+
+botaoCadastrar.onclick = function () {
+  toast.classList.add("visible");
+};
