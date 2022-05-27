2f723d77f82ed2b682d8b4e6943bd555396596d5
Adriano <98757084+AdrianoFront@users.noreply.github.com>
2022-05-26 21:51:00 -0300

    button add box with music

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let toast = document.querySelector(".toast-container");
+let toast = document.querySelector(".toast");
 let botaoCadastrar = document.querySelector("#botaoCadastrar");
 console;
 
 botaoCadastrar.onclick = function () {
   toast.classList.add("visible");
+
+  setTimeout(function () {
+    toast.classList.remove("visible");
+  }, 2000);
 };
