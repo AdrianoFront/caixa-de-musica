63aea432d8880d15e73d9427405c42d29d827bb5
Adriano <98757084+AdrianoFront@users.noreply.github.com>
2022-05-27 22:11:40 -0300

    refatoring buttons and add new styles

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let toast = document.querySelector(".toast");
-let botaoCadastrar = document.querySelector("#botaoCadastrar");
+let botaoPlay = document.querySelector("#botaoPlay");
 console;
 
-botaoCadastrar.onclick = function () {
+botaoPlay.onclick = function () {
   toast.classList.add("visible");
 
   setTimeout(function () {
     toast.classList.remove("visible");
   }, 2000);
+};
+
+let permanent = document.querySelector(".permanent");
+let botaoPlay2 = document.querySelector("#botaoPlay2");
+console;
+
+botaoPlay2.onclick = function () {
+  permanent.classList.add("visible");
+
+  setTimeout(function () {
+    permanent.classList.remove("visible");
+  }, 6000);
 };
